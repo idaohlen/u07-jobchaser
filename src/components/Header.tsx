c49b31412a7134ef873c97113cf2dec836a7435d
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2025-02-19 18:25:11 +0100

    redesign header and create a component for the mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,35 @@
+'use client';
+
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@/store/rootReducer';
 import { logout } from '@/store/slices/authSlice';
 
 import { useTheme } from 'next-themes';
+import { useRouter } from 'next/navigation';
 
 import Link from 'next/link';
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, Tooltip } from '@heroui/react';
+import { Icon } from '@iconify/react';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, Button, Tooltip } from '@heroui/react';
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@heroui/react";
 
 import ThemeSwitcher from '@/components/ThemeSwitcher';
 import BookmarkedJobs from '@/components/BookmarkedJobs';
+import MobileMenu from '@/components/MobileMenu';
 
 export default function Header() {
+  const router = useRouter();
   const { theme } = useTheme();
   const dispatch = useDispatch();
   const { isAuthenticated, user } = useSelector((state: RootState) => state.auth);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <Navbar className='fixed top-0 left-0 right-0 z-50 bg-white dark:bg-gray-900'>
+    <Navbar isBordered className='fixed top-0 left-0 right-0 z-50 bg-white dark:bg-gray-900' isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+      <NavbarMenuToggle
+        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        className="sm:hidden"
+      />
       <NavbarBrand>
         <Button
           className='font-bold text-inherit text-lg'
@@ -28,35 +41,36 @@ export default function Header() {
           JobChaser
         </Button>
       </NavbarBrand>
+
       <NavbarContent justify='end'>
-        <NavbarItem className=''>
-          <Link href="/jobs">Jobs</Link>
-        </NavbarItem>
-        { isAuthenticated
-          ? (
-            <>
-              {user?.name}
-              <Button color='primary' variant='flat' onPress={() => dispatch(logout())}>
-                Logout
-              </Button>
-            </>
-          ) : (
-            <>
-              <NavbarItem className='hidden lg:flex'>
-                <Link href="/signin">Login</Link>
-              </NavbarItem>
-              <NavbarItem>
-                <Button as={Link} color='primary' href='/signup' variant='flat'>
-                  Sign Up
-                </Button>
-              </NavbarItem>
-            </>
-          )
-        }
+        <NavbarItem className='hidden sm:flex'><Button variant='light' onPress={() => router.push('/jobs')}>Jobs</Button></NavbarItem>
+
+        {/* User account */}
+        <Dropdown>
+          <DropdownTrigger>
+            <Button className='hidden sm:flex' variant='light' startContent={<Icon icon='material-symbols:account-circle' className='text-2xl pointer-events-none flex-shrink-0'/>}>
+            { isAuthenticated ? user?.name : 'Log In or Sign Up' }
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Account">
+            { isAuthenticated
+              ? (
+                  <DropdownItem key="account-logout" onPress={() => dispatch(logout())}>Log Out</DropdownItem>
+              ) : (
+                <>
+                  <DropdownItem key="account-signin" onPress={() => router.push('/signin')}>Log In</DropdownItem>
+                  <DropdownItem key="account-signup" onPress={() => router.push('/signup')}>Sign Up</DropdownItem>
+                </>
+              )
+            }
+          </DropdownMenu>
+        </Dropdown>
+
         {/* Display bookmarked jobs */}
         <NavbarItem>
           <BookmarkedJobs />
         </NavbarItem>
+
         {/* Theme switcher toggle */}
         <NavbarItem>
           <Tooltip content={`Switch to ${theme === 'dark' ? 'light' : 'dark'} mode`} showArrow={true}>
@@ -66,7 +80,11 @@ export default function Header() {
             </div>
           </Tooltip>
         </NavbarItem>
+
       </NavbarContent>
+      
+      {/* Mobile menu  */}
+      <MobileMenu closeMenu={() => setIsMenuOpen(false)} />
     </Navbar>
   )
 }
---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@/store/rootReducer';
+import { useRouter } from 'next/navigation';
+import { logout } from '@/store/slices/authSlice';
+
+import { NavbarMenu, NavbarMenuItem, Button } from "@heroui/react";
+
+export default function MobileMenu({closeMenu}: {closeMenu: () => void}) {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const menu = [
+    {
+      label: 'Jobs',
+      action() {
+        router.push('/jobs');
+      }
+    },
+    {
+      label: 'Log In',
+      authState: 'loggedOut',
+      action() {
+        router.push('/signin');
+      }
+    },
+    {
+      label: 'Sign Up',
+      authState: 'loggedOut',
+      action() {
+        router.push('/signup');
+      }
+    },
+    {
+      label: 'Log Out',
+      authState: 'loggedIn',
+      action() {
+        dispatch(logout());
+      }
+    }
+  ];
+  
+  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+
+  return (
+    <NavbarMenu>
+      {menu.map(item => {
+        if (isAuthenticated && item.authState === 'loggedOut') return null;
+        if (!isAuthenticated && item.authState === 'loggedIn') return null;
+
+        return (
+          <NavbarMenuItem key={item.label}>
+            <Button
+              variant='light'
+              radius='sm'
+              className='w-full'
+              onPress={() => {
+                item.action();
+                closeMenu();
+              }}
+            >
+              {item.label}
+            </Button>
+          </NavbarMenuItem>
+        )
+      })}
+    </NavbarMenu>
+  )
+}
