48cf9a14fe77a66fee7ba4aecf59bae171248590
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2025-02-21 11:31:14 +0100

    JobCard mobile design adjustments

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -20,7 +20,7 @@ export default function JobCard({data}: {data: Job}) {
     <>
       <Card key={data.id} className='w-full' radius='sm'>
         <CardBody>
-          <div className='flex gap-1 items-center mb-2'>
+          <div className='flex gap-1 justify-between items-center mb-2'>
             <Image
               src={data.company_logo}
               height={40}
@@ -28,20 +28,26 @@ export default function JobCard({data}: {data: Job}) {
               radius='sm'
               alt={`Company logo for ${data.company_name}`}
             />
+            {/* Title and Company name */}
             <div className='flex flex-col text-left ml-2 mr-auto'>
               <p className='text-md font-semibold'>{data.title}</p>
               <p className='text-small text-default-00'>{data.company_name}</p>
             </div>
-            <div className='text-xs text-slate-400 dark:text-slate-600'>
-              { formatDistanceToNow(new Date(data.publication_date), { addSuffix: true }) }
+            {/* Publication date and bookmark button */}
+            <div className='contents'>
+              <div className=' max-sm:hidden text-xs text-slate-400 dark:text-slate-600'>
+                { formatDistanceToNow(new Date(data.publication_date), { addSuffix: true }) }
+              </div>
+              <BookmarkButton jobId={data.id.toString()} />
             </div>
-            <BookmarkButton jobId={data.id.toString()} />
           </div>
-          <div className='flex justify-between gap-4'>
-            <div className='flex items-center gap-2 text-xs text-slate-800 dark:text-slate-400'>
-              <Chip variant='dot' size='sm' color='primary'>{data.category}</Chip>
-              <Chip variant='dot' size='sm' color='warning'>{data.job_type.replace('_', ' ')}</Chip>
-              <p className='truncate'>{data.candidate_required_location}</p>
+          <div className='flex flex-col sm:flex-row justify-between gap-4'>
+            <div className='flex flex-col sm:flex-row items-start sm:items-center gap-2 text-xs text-slate-800 dark:text-slate-400'>
+              <div className='flex sm:contents gap-2'>
+                <Chip variant='dot' size='sm' color='primary'>{data.category}</Chip>
+                <Chip variant='dot' size='sm' color='warning'>{data.job_type.replace('_', ' ')}</Chip>
+              </div>
+              <p className='truncate w-full'>{data.candidate_required_location}</p>
             </div>
             <Button variant='ghost' size='sm' onPress={handleOpenModal}>Read more</Button>
           </div>
